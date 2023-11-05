5e57caa47d2760baf440e4c8d584d8d300549a89
IrnkvezzDeveloper <105006743+IrnkvezzDeveloper@users.noreply.github.com>
2023-11-05 17:34:59 +0000

    незначительные изменения

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,5 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-  path(''       , views.index,  name='index'),
-  path('tables/', views.tables, name='tables'),
+  path(''       , views.index,  name='index')
 ]
